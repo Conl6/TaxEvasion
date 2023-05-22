046b322dad5a8bad1b8c9630f2444cdcc54b761d
Conl6 <121958597+Conl6@users.noreply.github.com>
2023-05-22 14:35:59 +0100

    Start of Player Movement & Basic Cam Mechanics
    Set up Unity Environment, Start to code basic Player Movement & Implemented the basic Camera Controls.

---
+++ Tax Evasion/Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float mouseSensitivity;
+
+    public Transform camHolder;
+    public Transform Orientation;
+
+    float yRotation;
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        private void Update()
+        {      
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * mouseSensitivity;
+
+            yRotation += mouseY;
+
+            camHolder.rotation = Quaternion.Euler(0, yRotation, 0);
+        }
+}
+
---
+++ Tax Evasion/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float moveSpeed = 2;
+    Vector3 moveDirection;
+
+    public float groundDrag;
+
+    float horizontalInput;
+    float verticalInput;
+
+    [Header("Refrence")]
+    public Transform orientation;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        rb.drag = groundDrag;
+    }
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void PlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+}
